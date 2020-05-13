081c9d0dc0f1110f447856eb3d63efcaa155705a
phuocnguyen <nphuocnguyen2000@gmail.com>
2020-05-13 13:20:43 +0700

    Done pls

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 
 const app = express()
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(bodyParser.json())
